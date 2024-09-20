3b19763094c745d0b9cc4b27d634beb7c0d38f06
zhangjun <ewalker@live.cn>
2024-09-20 10:02:41 +0800

    update

--- call_trtllm.sh
+++ call_trtllm.sh
@@ -4,10 +4,11 @@ token='ZmNlZmI0YjVjNmE5ZTY3YWZmNjQ3MGZlOTYwOGVkMzQwOGY2ZGM1ZQ=='
 
 # url="http://1893706806886638.cn-beijing.pai-eas.aliyuncs.com/api/predict/${service_name}/generate"
 url="localhost:8000/generate"
+url="localhost:8000/v1/completions"
 
 # curl -v -X GET http://1893706806886638.cn-beijing.pai-eas.aliyuncs.com/api/predict/${service_name}/health -H "Authorization: Bearer ${token}"
 
 curl -v -X POST -d \
-    '{"prompt": "The capital of France is","max_new_tokens": 256,"temperature": 0.0,"streaming": false}' \
+    '{"prompt": "The capital of France is","max_tokens": 256,"temperature": 0.80,"stream": false, "top_p":1, "top_k":3, "repetition_penalty":1.08, "length_penalty":1.0, "model":"trtllm"}' \
     --header "Content-Type: application/json" $url \
     -H "Authorization: Bearer ${token}" -s -w %{time_total}
--- example/main.py
+++ example/main.py
@@ -8,16 +8,20 @@ from tensorrt_llm.hlapi.tokenizer import TransformersTokenizer
 from tensorrt_llm.hlapi.llm_utils import QuantConfig
 from tensorrt_llm.quantization.mode import QuantAlgo
 
+tp_size, pp_size = 4, 2
 # model could accept HF model name or a path to local HF model.
-model_path = "/mnt/data/shared/test/aistory/8b_llama3_as_en_aug_v3_0807"
-engine_path = "/opt/trtllm/"
+model_path = "/mnt/data/zhangjun/mydev/trtllm_tp1pp1_0905"
+tokenizer_path = "/mnt/data/shared/test/aistory/8b_llama3_as_en_aug_v3_0807"
+model_path = "/mnt/models/huggingface/hub/models--Qwen--Qwen2-72B-Instruct/snapshots/1af63c698f59c4235668ec9c1395468cb7cd7e79/"
+tokenizer_path = "/mnt/models/huggingface/hub/models--Qwen--Qwen2-72B-Instruct/snapshots/1af63c698f59c4235668ec9c1395468cb7cd7e79/"
+engine_path = f"/mnt/zhangjun/mydev/qwen72b_tp{tp_size}pp{pp_size}/"
 
-max_running = 8     # max_batch_size
+max_running = 16     # max_batch_size
 max_beam_width = 1
-max_seq_len = 1024  # max_seq_len 512
+max_seq_len = 8192  # max_seq_len 512
 build_config = BuildConfig(max_batch_size=max_running, max_beam_width=max_beam_width, max_seq_len=max_seq_len)
 
-quantization = None # "fp8"
+quantization = "FP8" # "FP8", None
 # Set the compute quantization.
 quant_algo = QuantAlgo(quantization) if quantization is not None else None
 quant_config = QuantConfig()
@@ -33,19 +37,24 @@ quant_config.kv_cache_quant_algo = quant_algo.value \
 build_config.plugin_config.use_paged_context_fmha = True
 build_config.plugin_config.use_fp8_context_fmha = True \
         if quant_algo == QuantAlgo.FP8 else False
-llm = LLM(model=engine_path, tensor_parallel_size=1, pipeline_parallel_size=1)
+llm = LLM(model=model_path,
+    tokenizer=tokenizer_path,
+    # enable_build_cache=True,
+    tensor_parallel_size=tp_size,
+    pipeline_parallel_size=pp_size,
+    build_config=build_config,
+    quant_config=quant_config)
 
-
-build_engine = False
+build_engine = True
 if build_engine:
     # save engine
     llm.save(engine_path)
     sys.exit(1)
 # Sample prompts.
 prompts = [
-    "Hello, my name is",
-    "The president of the United States is",
-    "The capital of France is",
+    # "Hello, my name is",
+    # "The president of the United States is",
+    # "The capital of France is",
     "The future of AI is",
 ]
 
@@ -57,11 +66,15 @@ sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 async def task(id: int, prompt: str):
 
     # streaming=True is used to enable streaming generation.
-    async for output in llm.generate_async(prompt,
-                                           sampling_params,
-                                           streaming=True):
+    # async for output in llm.generate_async(prompt,
+    #                                        sampling_params,
+    #                                        streaming=True):
+    generator = llm.generate_async(prompt,
+                                    sampling_params,
+                                    streaming=True)
+    async for output in generator:
         print(f"Generation for prompt-{id}: {output.outputs[0].text!r}")
-        print(output.outputs[0])
+        print(output)
 
 
 async def main():
